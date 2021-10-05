39c7fade30701d030b016711315c8db4d6a0abb4
Zohaib <zohaibmanzoor1700@gmail.com>
2021-10-05 14:54:10 +0000

    Completed Code

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 //1
-function fizzBuzz(){
-  
+const fizzBuzz = () => {
+  for (let i = 1; i <= 100; i++){
+      if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz")
+      } else if (i % 5 === 0) {
+        console.log("Buzz")
+      } else if (i % 3 === 0 ) {
+        console.log("Fizz")
+      } else {
+        console.log(i)
+      }
+  }
 }
-
+//fizzBuzz()
 //2
-function fizzBuzz(number){
-  
+const fizzzBuz = (number) => {
+    let answer = [];
+    for (let i = 1; i <= number; i++){
+        if (i % 3 === 0 && i % 5 === 0){
+        answer.push("FizzBuzz")
+      } else if (i % 5 === 0) {
+        answer.push("Buzz")
+      } else if (i % 3 === 0 ) {
+        answer.push("Fizz")
+      } else {
+        answer.push(i.toString())
+      }
+    
+  } console.log(answer)
 }
 
-
+fizzzBuz(15)
